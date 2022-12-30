542d9c961ba9c3a435b30952986c06bbba125193
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2022-12-29 16:39:20 -0800

    pass test 1 of parts 1, 2, 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(str) {
+    let arr = str.split(' ');
+    let res = [];
+    for (let i = arr.length; i >= 0; i--) {
+        res.push(arr[i]);
+    }
+    
+    return res.join('');
+}
 // Do not edit below this line
 module.exports = reverseString;
