a9b04f4bc37ff41943346058ee2b6681571c85b9
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2022-12-29 17:34:12 -0800

    finish 3 & 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
 const reverseString = function(str) {
-    let arr = str.split(' ');
+    let arr = str.split('');
     let res = [];
     for (let i = arr.length; i >= 0; i--) {
         res.push(arr[i]);
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(...args) {
+    arr = args[0];
+    newArr = [];
+    arr.forEach(i => {
+        if (!args.includes(i)) {
+            newArr.push(i);
+        }
+    });
+    return newArr;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
