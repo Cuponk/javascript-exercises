f52a267b32fe17eeb99a376a11f94f9bdf361dfa
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2022-12-29 17:45:02 -0800

    finish 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let max = 0;
+    let min = 0;
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (num1 < num2) {
+        max = num2;
+        min = num1;
+    }
+    else {
+        max = num1;
+        min = num2;
+    }
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
